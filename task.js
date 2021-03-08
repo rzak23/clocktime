78461c682c931d80984878c1c490d468419e8909
rzak23 <reza.akbar2396@gmail.com>
2021-03-08 18:18:19 +0800

    tambah menu

--- task.js
+++ task.js
@@ -1,4 +1,50 @@
-$(document).ready(function(){ // startTimer();
+$(document).ready(function(){
+    jam_now();
+
+    $('.nav-link').click(function(){
+        var menu = $(this).attr('data');
+
+        if(menu == "clock"){
+            $('.nav-link').removeClass('active');
+            $(this).addClass('active');
+            $('#timer').hide(500);
+            $('#clock').show(500);
+        }else if (menu == "timer"){
+            $('.nav-link').removeClass('active');
+            $(this).addClass('active');
+            $('#clock').hide(500);
+            $('#timer').show(500);
+        }
+        $('#clock').show();
+    })
+
+
+    setInterval(jam_now,1000);
+    $('#timer').hide();
+
+    function jam_now(){
+        var d = new Date();
+
+        var year = d.getFullYear();
+        var month = d.getMonth();
+        var day = d.getDate();
+        var hour = d.getHours();
+        var minute = d.getMinutes();
+        var sec = d.getSeconds();
+
+        var watch = hour+":"+minute+":"+sec;
+        var calendar = day+"/"+month+"/"+year;
+
+        $('#show_watch').html(watch);
+        $('#show_cal').html(calendar);
+    }
+
+    function openTimer(){
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        $('#clock').hide(500);
+        $('#timer').show(500);
+    }
 
     $('#btnStart').click(function(){
         var min = $('#minute').val();
