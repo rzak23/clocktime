501fb03e7a7b87465448cf05f46bc164880e5a91
Reza Akbar Hidayat <50098895+rzak23@users.noreply.github.com>
2021-04-15 21:53:47 +0800

    frameles

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow} = require('electron');
 require('update-electron-app')({
   updateInterval: '1 hour'
 })
@@ -7,11 +7,12 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 1080,
     height: 400,
+    frame: false,
     icon: __dirname + '/build/icon.png',
   })
 
   win.loadFile('index.html');
-  win.removeMenu();
+  // win.removeMenu();
 }
 
 app.whenReady().then(createWindow)
